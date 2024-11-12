d55b5f28fedfb169184d933219e46cd92af4c1e9
Louis Muriuki <luihugo247@gmail.com>
2024-11-12 09:34:45 +0300

    interfaces1

---
+++ Interfaces/src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    println("Hello World!")
+}
+// comonly used behaviour shared among diff classes, you dont want to provide the actual code that goes into those
+// classes to implement the interface.
+
+// cannot have constructors-> they cannot be instantiated only implemented.
+interface Engine{
+    fun startEngine()
+}
+
+class Car(val name:String, val color:String):Engine{
+    override fun startEngine() {
+        println("The car is starting the engine")
+    }
+
+}
+
+class Truck(val name:String, val color:String):Engine{
+    override fun startEngine() {
+        println("The truck is starting the engine")
+    }
+
+}
+class Plane(val name:String, val color:String):Engine{
+    override fun startEngine() {
+        println("The plane is starting the engine")
+    }
+
+}
