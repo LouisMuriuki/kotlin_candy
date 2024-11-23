7d05e4e68992f576f1eaa7a8714c852cce531bb4
Louis Muriuki <luihugo247@gmail.com>
2024-11-23 12:41:15 +0300

    delegation

---
+++ Delegation/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+
+    // inheritance anables inheritance from only one class.
+    // with delegationyou can inherit from multiple classes.
+    println("Hello World!")
+}
---
+++ Interface1/src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    println("Hello World!")
+    //each button now has its own impl of the onpress
+
+    //anonymous class ->object:etc (it is anonymous coz its instantiated and created at the same time)
+
+    val loginButtton= Button("login",1245,object:OnPress{
+        override fun handleClick() {
+            TODO("Not yet implemented")
+        }
+
+    })
+
+    val signupButton=Button("Signup", 245,object:OnPress{
+        override fun handleClick() {
+            TODO("Not yet implemented")
+        }
+
+    })
+}
+
+class Button(var text:String, var id:Int,OnPress:OnPress){
+
+}
+
+
+interface OnPress{
+    fun handleClick()
+
+}
