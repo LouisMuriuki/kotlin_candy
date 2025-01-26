6b7d72e7101538541503ce43e33899e8dd246506
Louis Muriuki <luihugo247@gmail.com>
2025-01-26 11:13:19 +0300

    kotlin ds

---
+++ Arrays/src/main/kotlin/Main.kt
@@ -0,0 +1,86 @@
+//using primitives in an object array is not recommend as the primitives will be boxed
+//boxing introduces perf overhead.
+//use arrays for performance gains else use collections
+//when to use collections instead
+//->when you need readonly data.
+//whjen you are going to be addd nd removing regulartly, array are fixed in sze, create a new one each time which is inefficient.
+
+fun main() {
+    println("Hello World!")
+
+    val numbers =arrayOf<Int>(1,2,3)
+    println(numbers.joinToString())
+    val empty= emptyArray<Int>()
+    val nulls:Array<Int?> = arrayOfNulls(3)
+    ;
+
+    val squares=Array(5){i->(i*i).toString()}
+    val squares1=Array(5){(it*it).toString()}
+    println(squares.joinToString())
+
+    println(nulls.joinToString())
+
+
+    //nested arrays
+    //2DArray
+    val twoDArray = Array(2){ Array<Int>(2) { 0 } }
+    println(twoDArray.contentDeepToString())
+
+    //3DArray
+    val threeeDarray=Array(1){Array(2){Array(1){-1} } }
+
+    println(threeeDarray.contentDeepToString())
+
+    //arrays are always accesible using indexes
+
+//vararg/varargs-variable number of arguments
+    fun spitArrays(vararg numbers: Array<Int>){
+        try {
+            if(numbers.isEmpty()){
+                return println("pass some arguments please")
+            }
+            numbers.forEach { println(it.joinToString()) }
+        }catch(e:Exception) {
+            println("error is $e")
+        }
+
+
+
+    }
+
+spitArrays(numbers)
+    print("I ran")
+
+
+    //compare arrays
+    val simpleArray=arrayOf<Int>(1,2,3)
+    val arary2=arrayOf<Int>(1,2,3)
+    val anotherArray=arrayOf<Int>(1,2,3)
+
+    // Compares contents of arrays
+    //infix functions don't require a dot
+    println(simpleArray contentEquals anotherArray)
+
+
+//    Array transformation
+
+    println(numbers.sum())
+    println(numbers.shuffle()) //shuffles elements in array
+
+    println(numbers.toList())
+    println(numbers.toSet())
+
+//    to map
+//only an array of Pair<KV> can be converted to a map
+//    first value becomes key and the second becomes a value
+//    keys must be unique and the latest key replaces any existing one
+
+    val data = arrayOf(1 to "Lilian",2 to "Muriuki",1 to "Michele")
+
+println(data.toMap())
+
+    var jdcvd=IntArray(3){ it+2 }
+
+    println(jdcvd.joinToString())
+
+}
--- Collections/src/main/kotlin/Main.kt
+++ Collections/src/main/kotlin/Main.kt
@@ -1,38 +1,80 @@
+//lists-> ordered collections of items, not necessarirly unique.
+//Sets-> Unique unordered collection of items
+//since they are unordered you cant use the index->all other list method can be used->in,count,add,remove
+//Maps->sets of key value pais that are unique and map to only one value,
+//allow you to look up a value without using a numbered index.
+//keys must be unique so as to get that particular value you want
+
+
+
+
 fun main() {
-//    ListExample();
-    mapExamples();
+    ListExample();
+//    mapExamples();
 }
 
 fun mapExamples() {
 //mutableMapOf<>() ->you can add or remove items to map;
     val users = mutableMapOf<Int, String>(1 to "Louis", 2 to "Hugo", 3 to "Martial")
+    var names= mutableMapOf(1 to "one",2 to "two", 3 to "three")
+    // to prevent unwanted modification of your map, you can cast it.->casting(remember?, check list above)
+//    you can access item by using indexing
+    println(names[1])
+//    you can also add items through indexing
     users[5] = "Peter"
+
     users.remove(3)
     users.forEach { (t, u) ->
         println("$t and $u")
     }
+    // to check if a key is in the map user .containsKey()
+//    names.containsKey(2)
+
+        //get all key -> names.keys
+        //get all values -> names.values
+
+//    check if a value is in the keys
+      println(2 in names.keys)
 
 }
 
 
 fun ListExample() {
-    val names = listOf<String>("Louis", "Lorna", "Kamau");
+    val names = listOf<String>("Louis", "Lorna", "Kamau"); //readonly
 
     //mutablelist accepts  duplicate items
-    val namesmaster = mutableListOf("Louis", "Lorna", "Kamau", "Louis");
-    val nameSet = setOf("Louis", "Lorna", "Kamau", "Louis");
+
+    val namesMaster = mutableListOf("Louis", "Lorna", "Kamau", "Louis"); //mutable
+    //create a unmodifiable val to hold a list of mutable lists-> Known as casting
+    val nameBoss= listOf(namesMaster)
+
+    //lists are ordered so to access them we use the index access operator[]
+    println(namesMaster[1])
+//    println(namesMaster.first())
+
+    //.count to get number of items in a list
+    println(namesMaster.count())
+
+    //.in to checkk if an item is in the list
+    println("Louis" in namesMaster)
+
+    //.add() and .remove()
+
+
+    val nameSet = setOf("Louis", "Lorna", "Kamau", "Louis");//even though we gat 2 luis, only one is printed
     val namesmasterSet = mutableSetOf("Louis", "Lorna", "Kamau", "Louis");
+    println(nameSet)
 
 
 
-    namesmaster.add("name2")
+    namesMaster.add("name2")
 
-    namesmaster.remove("name2")
+    namesMaster.remove("name2")
 
 
-    namesmaster.forEach { println(it) }
+//    namesMaster.forEach { println(it) }
     // does not accept duplicates-last LOUIS WONT BE PRINTED
-    namesmasterSet.forEach { println(it) }
+//    namesmasterSet.forEach { println(it) }
 
 
     // set uses the equality(referential/structural) operator to determine whether there are duplicates or not.
@@ -48,7 +90,7 @@ fun ListExample() {
     //without data class duplicates will be available,
     // with data class no duplicates
 
-    anotherSet.forEach { println(it.name) }
+//    anotherSet.forEach { println(it.name) }
 
 }
 
